e5cfa11fb2b4b2a8e6c86a292068cc6e7e25f35a
unknown <eslaamfodaa@gmail.com>
2022-12-08 13:25:49 +0200

    update comp data

--- hooks/useCompData.js
+++ hooks/useCompData.js
@@ -281,22 +281,23 @@ const useCompData = (compName) => {
   const contactData = {
     title: "<h1 class='text-4xl font-semibold'>Contact Us</h1>",
     subTitle: "<p>Our team is always available to assist you.</p>",
+    btn:"<span>Send</span>",
     contacts: [
       {
         id: 1,
-        iconName:"email",
+        iconName: "email",
         title: "<h1 class='4xl font-semibold'>email</h1>",
         details: "<p>info@business.com</p>",
       },
       {
         id: 2,
-        iconName:"phone",
+        iconName: "phone",
         title: "<h1 class='4xl font-semibold'>Phone</h1>",
         details: "<p>1(800) - 374 - 990</p>",
       },
       {
         id: 3,
-        iconName:"address",
+        iconName: "address",
         title: "<h1 class='4xl font-semibold'>Address</h1>",
         details: "<p>301 W. 41st Street, Suite 501 Miami Beach, FL 33140</p>",
       },
