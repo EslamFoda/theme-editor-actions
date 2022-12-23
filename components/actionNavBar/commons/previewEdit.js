caa770210bf20990176a7ebe6296a9b564992301
unknown <eslaamfodaa@gmail.com>
2022-12-23 17:20:29 +0200

    add debounce to edit mode button

--- components/actionNavBar/commons/previewEdit.js
+++ components/actionNavBar/commons/previewEdit.js
@@ -1,23 +1,26 @@
 import { updateDoc } from "firebase/firestore";
 import { FaEdit, FaEye } from "react-icons/fa";
+import { debounce } from "debounce";
 
 const PreviewEdit = ({ editSections, themeData }) => {
+  const toggleEdit = async () => {
+    await updateDoc(themeData, {
+      editSections: !editSections,
+      compName: "",
+      editFiles: false,
+      editImg: false,
+      colorsEdit: false,
+      fontEdit: false,
+      editEffects: false,
+      stylesEditing: false,
+      addSection: false,
+    });
+  };
+  let delayEdit = debounce(toggleEdit, 700);
   return (
     <div
       className=" lg:mx-4 md:mx-4 mx-2 justify-self-end cursor-pointer"
-      onClick={async () => {
-        await updateDoc(themeData, {
-          editSections: !editSections,
-          compName: "",
-          editFiles: false,
-          editImg: false,
-          colorsEdit: false,
-          fontEdit: false,
-          editEffects: false,
-          stylesEditing: false,
-          addSection: false,
-        });
-      }}
+      onClick={delayEdit}
     >
       {editSections ? (
         <div className="flex text-[#98A2B3] hover:text-white items-center gap-3">
